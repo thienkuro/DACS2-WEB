0787c4d9c50dfc6971cd5d89495e81143e733c55
LEGION <dqdbs06@gmail.com>
2025-11-25 00:26:21 +0700

    dang sua

--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Đăng ký
 router.post('/register', async (req, res) => {
